592d6f65e6bef0e342224e260d8da681ee157f77
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2023-09-24 13:55:05 +0200

    Commit test for array of 5 links, infrastructure created in for-loop

--- src/main/java/d22_09_2023/SeleniumTestExample3.java
+++ src/main/java/d22_09_2023/SeleniumTestExample3.java
@@ -1,4 +1,11 @@
 package d22_09_2023;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+
 //Napisati program koji:
 //ima niz od 5 stringova. Svaki element u nizu je url stranice:
 //https://www.google.com/
@@ -13,7 +20,20 @@ package d22_09_2023;
 //pravi pauza od 1s
 //ponistava testna stuktura
 public class SeleniumTestExample3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<String> linkovi = new ArrayList<>();
+        linkovi.add("https://www.google.com/");
+        linkovi.add("https://www.facebook.com/");
+        linkovi.add("https://www.youtube.com");
+        linkovi.add("https://www.ebay.com/");
+        linkovi.add("https://www.katalon.com/");
 
+        for (int i = 0; i < linkovi.size(); i++) {
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+            driver.get(linkovi.get(i));
+            Thread.sleep(1000);
+            driver.quit();
+        }
     }
 }
