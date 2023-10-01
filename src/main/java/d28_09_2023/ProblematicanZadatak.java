f0e141aa0d8abc256ab408a2be16f1e0763a5b1d
Ksenija Beocanin <ksenijabeocanin@gmail.com>
2023-10-01 11:28:17 +0200

    Add corrected code

---
+++ src/main/java/d28_09_2023/ProblematicanZadatak.java
@@ -0,0 +1,38 @@
+package d28_09_2023;
+
+//Za zadatak iz foldera za domaci izdebagirati i ispraviti da radi.
+
+    import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ProblematicanZadatak {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("http://seleniumdemo.com/?product=git-basics");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+        WebElement addToCartBtn = driver.findElement(By.name("add-to-cart"));
+
+        addToCartBtn.click();
+
+        Thread.sleep(1000); // cekaj da se osvezi stranica
+
+        wait.until(ExpectedConditions.textToBe(By.xpath("//*[@id=\"tc-page-wrap\"]//a/sup"), "1"));
+
+        System.out.println("KRAJ");
+
+        driver.quit();
+    }
+}
